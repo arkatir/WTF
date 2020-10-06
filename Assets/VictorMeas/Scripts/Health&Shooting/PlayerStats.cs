bdcf31e3cf8356e4208d4618443f57e086cf2930
vickyconcarne <39057304+vickyconcarne@users.noreply.github.com>
2020-10-06 12:00:22 +0200

    ajouté health, en train de bosser sur projectile, fini pool manager

---
+++ Assets/VictorMeas/Scripts/Health&Shooting/PlayerStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private int maxHealth;
+    [SerializeField]
+    private int health;
+    void Start()
+    {
+        
+    }
+
+    public void AddHealth(int val)
+    {
+        health += val;
+    }
+
+    public void RemoveHealth(int val)
+    {
+        int removedVal = health - val;
+        if (removedVal < 0)
+        {
+            health = 0;
+        }
+        else
+        {
+            health = removedVal;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/VictorMeas/Scripts/ObjectPoolingScript/ObjectPoolManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Script dont le but est de donner un framework dans lequel on peut "creer" et "detruire" des objets en scène sans avoir 
+/// un impact négatif en performance à travers des Instanciate() et Destroy(). Un amas d'objets est instancié au début et 
+/// desactivé dans un coin, lkorsqu'on a besoin de "creer" un objet (mob, projectile, collectible): on récupère le premier
+/// objet non actif dans cette hierarchie et le teleporte au bon endroit, en ajustant les données de leur script. Pour "detruire" un objet, 
+/// on le desactive et on le range dans un coin.
+
+/// </summary>
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    [Header("Public Attributes")]
+    public static ObjectPoolManager managerInstance;
+
+    public List<GameObject> pooledObjects;
+
+    //General information on gameObj reference      
+    public GameObject objectToPool;
+    
+    public Transform storageTransform; //Where to put objects when inactive
+    [Header("Private Attributes")]
+    [SerializeField]
+    private int spawnRadius;
+    [SerializeField]
+    private int initialAmountToPool;
+    [SerializeField]
+    private float timeBetweenSpawn;
+    private float currentTime;
+
+    private void Awake()
+    {
+        managerInstance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Init
+        Vector3 storagePosition = storageTransform.position;
+        pooledObjects = new List<GameObject>();
+        for(int i=0; i < initialAmountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            //Hide and deactivate objects
+            obj.SetActive(false);
+            obj.transform.position = storagePosition;
+            //Add to initial pool
+            pooledObjects.Add(obj);
+        }
+    }
+
+    #region Public Methods
+
+    public GameObject GetAvailablePooledObject()
+    {
+        for(int i=0; i< pooledObjects.Count; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+    public GameObject CreateObject(Vector3 givenPosition,  Quaternion givenRotation)
+    {
+        GameObject obj = GetAvailablePooledObject();
+        if(obj != null)
+        {
+            obj.transform.position = givenPosition;
+            obj.transform.rotation = givenRotation;
+            obj.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void RemoveObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        obj.transform.position = storageTransform.position;
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
