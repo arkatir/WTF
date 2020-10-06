fe0789192ea4f71a90c156d36e190bce4b1e98d8
Aurélien Harcaut <aure92.web@free.fr>
2020-10-06 16:13:49 +0200

    Updated seek and destroy objects

--- Assets/Aurélien/Scripts/SpawnAutoDestroy.cs
+++ Assets/Aurélien/Scripts/SpawnAutoDestroy.cs
@@ -7,6 +7,8 @@ public class SpawnAutoDestroy : MonoBehaviour
     public GameObject autoDestroyPrefab;
     private AutoDestroy autoDestroy;
     private Vector3 spawnOffset = new Vector3(0, 0.8f, 0);
+    private int spawnNumber = 1;
+    public GameObject spawnPos;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +26,19 @@ public class SpawnAutoDestroy : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            GameObject spawn = Instantiate(autoDestroyPrefab, transform.position + spawnOffset, autoDestroyPrefab.transform.rotation);
-            autoDestroy = spawn.GetComponent<AutoDestroy>();
-            autoDestroy.target = other.gameObject;
+            float spawnRadius = (spawnNumber - 1) / 2;
+            float spawnAngle = (360) / spawnNumber;
+            spawnPos.transform.position = new Vector3(spawnRadius, 0, 0);
+            for (int i = 0; i < spawnNumber; i++)
+            {
+                GameObject spawn = Instantiate(autoDestroyPrefab, spawnPos.transform.position + spawnOffset, autoDestroyPrefab.transform.rotation);
+                autoDestroy = spawn.GetComponent<AutoDestroy>();
+                autoDestroy.target = other.gameObject;
+                autoDestroy.speedCap = Mathf.Sqrt(spawnNumber);
+                spawnPos.transform.RotateAround(Vector3.zero, Vector3.up, spawnAngle);
+            }
+            spawnNumber++;
+            transform.localScale *= 1.2f;
         }
     }
 }
