cf8150f5f7b97ed9d91321c9889e5bc72bd0e758
Martin Duvergey <martin.duvergey@gmail.com>
2020-10-06 17:44:20 +0200

    Wall gun

---
+++ Assets/Martin/Scripts/WallGun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class WallGun : MonoBehaviour
+{
+    public GameObject wallsPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(wallsPrefab, transform.position + transform.forward * 3 + Vector3.up * 2, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Martin/Scripts/Walls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Walls : MonoBehaviour
+{
+    public float fallingSpeed = 2f;
+    public float delayBeforeRelease = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FallDown");
+    }
+
+    private IEnumerator FallDown()
+    {
+        while (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Time.deltaTime * fallingSpeed, transform.position.z);
+            yield return null;
+        }
+        yield return new WaitForSeconds(delayBeforeRelease);
+        while (transform.position.y > -2f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Time.deltaTime * fallingSpeed, transform.position.z);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
