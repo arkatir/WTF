2fa4a32449546c10f1957e9f9a33021d5764f82e
Martin Duvergey <martin.duvergey@gmail.com>
2020-10-07 16:35:32 +0200

    DivideGun : limite le rétrécissement

--- Assets/Martin/Scripts/DivideGunProjectile.cs
+++ Assets/Martin/Scripts/DivideGunProjectile.cs
@@ -31,7 +31,7 @@ public class DivideGunProjectile : MonoBehaviour
         {
             int newHealth = enemyStats.GetHealth() / 2;
             enemyStats.SetHealth(newHealth > 0 ? newHealth : 1);
-            enemyStats.transform.localScale *= 0.75f;
+            enemyStats.transform.localScale = Vector3.one * Mathf.Max(0.75f * enemyStats.transform.localScale.x, 0.2f);
             //GameObject clone = Instantiate(other.gameObject, (transform.up * 2 + Random.Range(-1f, 1f) * transform.right) * spread, Quaternion.identity);
         }
         if (!other.TryGetComponent(out DivideGun divideGun) && !other.TryGetComponent(out PlayerStats playerStats))
