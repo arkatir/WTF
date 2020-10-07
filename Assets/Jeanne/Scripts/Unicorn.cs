cf163250f43d2f666b9d9e085ca9dadd9f2f77c8
Jeanne BERTOUX <jeannebertoux@gmail.com>
2020-10-07 16:36:37 +0200

    licorne pimper
    ajout timer
    ajout lumières

--- Assets/Jeanne/Scripts/Unicorn.cs
+++ Assets/Jeanne/Scripts/Unicorn.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class Unicorn : SlotItem
 {
-
+    private float timerDisable;
     public GameObject player;
     public Rigidbody rb;
+    public Transform handle;
 
     // Start is called before the first frame update
     void Start()
@@ -15,46 +17,40 @@ public class Unicorn : SlotItem
 
     }
 
-    //surcharger cette méthode de la classe SlotItem
-  /*  public void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        Debug.Log("Coucou");
-        if( other.gameObject.tag == "Player")
-        {
-            transform.parent.rotation = other.gameObject.transform.rotation;
-            transform.parent.position = other.gameObject.transform.position - (Vector3.up * 0.8f);
-            transform.parent.parent = other.gameObject.transform;
-            Camera cam = other.gameObject.transform.GetComponentInChildren<Camera>();
-            other.gameObject.transform.GetComponentInChildren<HeadBob>().enabled = false;
-            cam.transform.position += Vector3.up * 0.8f;
-            
-        }
+        timerDisable -= Time.deltaTime;
     }
-    */
 
     public override void OnInsert()
     {
+        if (timerDisable > 0) return;
+
+        GetComponent<MeshCollider>().enabled = false;
         rb.isKinematic = true;
-        transform.parent.rotation = player.gameObject.transform.rotation;
-        transform.parent.position = player.gameObject.transform.position - (Vector3.up * 0.5f);
-        transform.parent.parent = player.gameObject.transform;
+        transform.parent.rotation = player.transform.rotation;
+        player.transform.position = handle.position;
+        transform.parent.parent = player.transform;
         Camera cam = player.gameObject.transform.GetComponentInChildren<Camera>();
         player.gameObject.transform.GetComponentInChildren<HeadBob>().enabled = false;
         cam.transform.position += Vector3.up * 0.5f;
+        
 
        
     }
    
     public override void OnRemove()
     {
-        //on jette la licorne
+        timerDisable = 2;
+        //on jette la licorn
         rb.transform.position = rb.transform.position + rb.transform.forward * 1;
         rb.isKinematic = false;
-        player.transform.parent = null;
         player.SetActive(true);
         player.gameObject.transform.GetComponentInChildren<HeadBob>().enabled = true;
         Camera.main.transform.parent = player.transform;
         Camera.main.transform.position -= Vector3.up * 0.5f;
+        GetComponent<MeshCollider>().enabled = true;
+        transform.parent.parent = null;
 
 
     }
