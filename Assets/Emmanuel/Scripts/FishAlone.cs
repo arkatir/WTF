b525cf82b6a5c0abf9be97c4515a771c4c085a43
abracastoral <41753693+abracastoral@users.noreply.github.com>
2020-10-07 17:50:29 +0200

    Shader sur les poissons + attaque

--- Assets/Emmanuel/Scripts/FishAlone.cs
+++ Assets/Emmanuel/Scripts/FishAlone.cs
@@ -7,70 +7,113 @@ public class FishAlone : MonoBehaviour
     float speed;
     float startSpeed;
     float rotationSpeed = 1;
+    private bool notSeen = true;
 
     public float playerDistance;
 
     public GameObject[] flock;
-    GameObject[] noticedNeighbour;
+    public GameObject[] noticedNeighbour;
     public GameObject player;
 
-    //Vector3 averageHeading;
     Vector3 averagePosition;
-    //float neighbourDistance = 0.5f;
     public int noticedNeighbourNumber = 4;
 
-    private int c;
+    Material m_Material;
 
-    // Start is called before the first frame update
+    //countdown
+    private bool isCountingDown = false;
+    float timeAfterCountdown;
+
+    bool isDeadFish = true;
     void Start()
     {
+        player = GameObject.Find("Player");
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.SetFloat("_RandVal", Random.Range(0f, 6.28f));
+        m_Material.SetFloat("_Danger", 1f);
 
-        Material m_Material = GetComponent<Renderer>().material;
-        m_Material.SetFloat("_RandVal", Random.Range(0f,6.28f));
+    }
+
+    private void OnEnable()
+    {
+        timeAfterCountdown = 0;
+        isCountingDown = false;
         startSpeed = Random.Range(0.8f, 1.5f);
         speed = startSpeed;
         noticedNeighbour = new GameObject[noticedNeighbourNumber];
+        isDeadFish = false;
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        isDeadFish = true;
+    }
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, player.transform.position) > playerDistance)
+        if (!isDeadFish)
         {
-            if (Random.Range(0, 4) < 1)
+            if (Vector3.Distance(this.transform.position, player.transform.position) > playerDistance)
             {
-                ApplyRules();
+                if (notSeen)
+                {
+                    m_Material.SetFloat("_Danger", 1f);
+                    notSeen = false;
+                }
+                if (Random.Range(0, 4) < 1)
+                {
+                    ApplyRules();
+                    Debug.Log("test");
+                }
+                transform.position += transform.forward * speed * Time.deltaTime;
             }
-            transform.position += transform.forward * speed * Time.deltaTime;
+
+            else
+            {
+                if (!notSeen)
+                {
+                    notSeen = true;
+                    m_Material.SetFloat("_Danger", 0f);
+                }
+                this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - this.transform.position), rotationSpeed * Time.deltaTime * 2);
+                transform.position += transform.forward * speed * Time.deltaTime * 4;
+
+                if (Vector3.Distance(this.transform.position, player.transform.position) < 0.4f)
+                {
+                    isCountingDown = true;
+                }
+            }
+
+            if (isCountingDown)
+            {
+                timeAfterCountdown += Time.deltaTime;
+            }
+
+            if (timeAfterCountdown > 1.5)
+            {
+                this.gameObject.SetActive(false);
+                //deactivate fish
+            }
         }
-        else
-        {
-            //HuntPlayer();
-            //transform.position += transform.forward * speed * Time.deltaTime;
-            Vector3.Slerp(this.transform.position, player.transform.position, speed * Time.deltaTime * 3);
-        }
+
     }
 
+
+    //regle de flocking
     void ApplyRules()
     {
-        //Empty noticedNeighbour
         flock = FishSchool.fishSchool;
-        
-        //averageHeading = Vector3.zero;
+
         averagePosition = Vector3.zero;
-        
 
-        float groupSpeed = 0f;
         Vector3 goal = FishSchool.goal;
+        int c = 0;
 
-        c = 0;
-        
         //Choix des neigbours aléatoires
         GameObject neigbour;
-        while(c<noticedNeighbourNumber)
+        while (c < noticedNeighbourNumber)
         {
             neigbour = flock[Random.Range(0, flock.Length)]; // ne vérifie pas si les neighbours choisis sont différents
-            if(neigbour!=this.gameObject)
+            if (neigbour != this.gameObject)
             {
                 noticedNeighbour[c] = neigbour;
                 c += 1;
@@ -81,28 +124,16 @@ public class FishAlone : MonoBehaviour
         {
             averagePosition += fish.transform.position;
             FishAlone otherfish = fish.GetComponent<FishAlone>();
-            //groupSpeed += otherfish.speed;
         }
 
         averagePosition = averagePosition / noticedNeighbourNumber + (goal - this.transform.position);
-        //groupSpeed = groupSpeed / noticedNeighbourNumber;
-
         Vector3 direction = averagePosition - this.transform.position;
         float diffDir = Quaternion.Dot(transform.rotation, Quaternion.LookRotation(direction));
         this.speed = 0.3f + Mathf.Abs(diffDir * startSpeed);
         this.rotationSpeed = this.speed;
-        if (direction!= Vector3.zero)
+        if (direction != Vector3.zero)
         {
             this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
-        }
-    }
-
-    void HuntPlayer()
-    {
-        Vector3 towardPlayer = this.transform.position - player.transform.position;
-        if (towardPlayer != Vector3.zero)
-        {
-            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(towardPlayer), rotationSpeed * Time.deltaTime * 2);
         }
     }
 
