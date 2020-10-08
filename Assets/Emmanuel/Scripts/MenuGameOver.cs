77cad8dc91b8e31f7ed01f309dc60fe3d8576730
abracastoral <41753693+abracastoral@users.noreply.github.com>
2020-10-08 17:37:49 +0200

    ScriptGameOver

---
+++ Assets/Emmanuel/Scripts/MenuGameOver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+    public class MenuGameOver : MonoBehaviour
+    {
+        public GameObject menuOver;
+        public string sceneGame;
+        public GameObject player;
+
+        void Start()
+        {
+            //menuOver.SetActive(false);
+        }
+
+        void OnEnable()
+        {
+            player = GameObject.Find("Player");
+            player.GetComponent<RigidbodyFirstPersonController>().mouseLook.lockCursor = false;
+            ///Cursor.lockState = CursorLockMode.None;
+            //Cursor.visible = true;
+
+        }
+        void Update()
+        {
+        }
+        public void Replay()
+        {
+            Debug.Log("test");
+            SceneManager.LoadScene(sceneGame);
+        }
+
+        public void GameQuit()
+        {
+            Application.Quit();
+            Debug.Log("tes");
+
+        }
+    }
+
+}
