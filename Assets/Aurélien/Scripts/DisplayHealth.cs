4cdb2e81abc363479ba9aeb431ff98e40f0dae3d
Aurélien Harcaut <aure92.web@free.fr>
2020-10-08 11:47:46 +0200

    Title

---
+++ Assets/Aurélien/Scripts/DisplayHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayHealth : MonoBehaviour
+{
+    private PlayerStats playerstats;
+    private TextMeshProUGUI tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerstats = GetComponentInParent<PlayerStats>();
+        tmp = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp.text = "Health : " + playerstats.GetHealth();
+    }
+}
